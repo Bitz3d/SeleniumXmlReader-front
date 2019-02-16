f3684b787bc4967b55fc334b01a72aade971343f
Bitz3d <raf.cieslinski@gmail.com>
2019-02-16 14:20:48 +0100

    first commit

---
+++ src/Components/Body.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Body extends React.Component{
+    render(){
+        return(
+            <div className="mainContent">
+                <main>
+                    Body
+                </main>
+            </div>
+        )
+    }
+}
+
+
+export default Body
---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div className="navbar">
+                <header >This is header</header>
+            </div>
+        )
+    }
+}
+
+
+export default Header
